1b70673edb828d5856677b6c60eff24eacbc8cc5
Pyrotchnst <csundheim@gmail.com>
2022-10-27 16:32:24 -0600

    final

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -18,9 +18,8 @@ router
   .put(updateThought)
   .delete(deleteThought);
 
-router
-  .route("/:thoughtId/reactions")
-  .post(createReaction)
-  .delete(deleteReaction);
+router.route("/:thoughtId/reactions").post(createReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
