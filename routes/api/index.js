70845307d3b37cb21b265d96d4fd6c6ebc9f5495
Pyrotchnst <csundheim@gmail.com>
2022-10-27 13:26:11 -0600

    routes

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const users = require("./userRoutes");
+const thoughts = require("./thoughtRoutes");
+
+router.use("/users", users);
+router.use("/thoughts", thoughts);
+
+module.exports = router;
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  createThought,
+  getOneThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+router.route("/").get(getThoughts).post(createThought);
+
+router
+  .route("/:thoughtId")
+  .get(getOneThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
